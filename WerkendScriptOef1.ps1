9544654f1d01f9c2a27e21cf523e56858fa1bfed
LagaeJens <jens.lagae@student.howest.be>
2023-03-02 22:20:29 +0100

    shortend delays + rename

--- WerkendScriptOef1.ps1
+++ WerkendScriptOef1.ps1
@@ -37,25 +37,25 @@ try {
     # Set time zone
     Write-Host "Setting time zone ..."
     Set-TimeZone -Id "Central European Standard Time" -ErrorAction Stop
-    Start-Sleep -Seconds 5
+    Start-Sleep -Seconds 1
 
     # Enable remote desktop access
     Write-Host "Enabling remote desktop access ..."
     Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0 -ErrorAction Stop
-    Start-Sleep -Seconds 5
+    Start-Sleep -Seconds 1
 
 
     # Disable IE Enhanced Security Setting for Administrators
     Write-Host "Disabling IE Enhanced Security Setting for Administrators..."
     $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
     Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -ErrorAction Stop
-    Start-Sleep -Seconds 5
+    Start-Sleep -Seconds 1
 
     # Disable IE Enhanced Security Setting for Users
     Write-Host "Disabling IE Enhanced Security Setting for Users..."
     $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
     Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -ErrorAction Stop
-    Start-Sleep -Seconds 5
+    Start-Sleep -Seconds 1
 
 
     #first check if the icons are already set to small and else set them to small so the access is easier
@@ -64,12 +64,12 @@ try {
     }
     Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel" -Name "StartupPage" -Type DWord -Value 1
     Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ControlPanel" -Name "AllItemsIconView" -Type DWord -Value 1
-    Start-Sleep -Seconds 5
+    Start-Sleep -Seconds 1
 
     # Show file extensions in Windows Explorer
     Write-Host "Showing file extensions in Windows Explorer ..."
     Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Value 0 -ErrorAction Stop
-    Start-Sleep -Seconds 5
+    Start-Sleep -Seconds 1
 
     # Restart computer
     Write-Host "Restarting computer ..."
