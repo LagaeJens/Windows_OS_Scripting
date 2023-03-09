a5abf25efb763e927ea98801cb856fa65b5ec874
LagaeJens <jens.lagae@student.howest.be>
2023-03-09 10:56:54 +0100

    test

---
+++ Week_2/Script1.ps1
@@ -0,0 +1,26 @@
+# Prompt the user to enter the domain name and administrator credentials
+$DomainName = Read-Host "Enter the name of the new domain (e.g. example.com)"
+$DomainAdmin = Get-Credential -Message "Enter the domain administrator credentials"
+
+# Install the AD DS role
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+
+# Promote the server to a domain controller in a new forest and domain
+Install-ADDSForest `
+    -DomainName $DomainName `
+    -DomainNetbiosName $DomainName.Split('.')[0] `
+    -ForestMode Win2012R2 `
+    -DomainMode Win2012R2 `
+    -DomainAdministratorCredential $DomainAdmin `
+    -InstallDNS:$true `
+    -NoRebootOnCompletion:$false
+
+# Prompt the user to enter the DNS server IP addresses
+$DnsPrimary = Read-Host "Enter the IP address of the primary DNS server"
+$DnsSecondary = Read-Host "Enter the IP address of the secondary DNS server"
+
+# Set the DNS server IP addresses
+Get-NetAdapter | Where-Object { $_.InterfaceDescription -like "*Ethernet*" } | Set-DnsClientServerAddress -ServerAddresses ($DnsPrimary, $DnsSecondary)
+
+# Reboot the server to complete the domain controller installation
+Restart-Computer -Force
