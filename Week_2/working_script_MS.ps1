42f1b0505f2d4004ae433cf6544deb5d36478028
LagaeJens <jens.lagae@student.howest.be>
2023-03-12 20:44:55 +0100

    rename

---
+++ Week_2/working_script_MS.ps1
@@ -0,0 +1,63 @@
+# Prompt for hostname, local IP address, and DNS server addresses
+Add-Type -AssemblyName Microsoft.VisualBasic
+$hostname = [Microsoft.VisualBasic.Interaction]::InputBox("Enter the hostname for this server (e.g. DC01)", "Hostname")
+$localIP = [Microsoft.VisualBasic.Interaction]::InputBox("Enter the local IP address for this server (e.g. 192.168.0.10)", "Local IP address")
+$defaultGateway = [Microsoft.VisualBasic.Interaction]::InputBox("Enter the default gateway IP address for this server (e.g. 192.168.0.1)", "Default gateway IP address")
+$dnsServers = [Microsoft.VisualBasic.Interaction]::InputBox("Enter the DNS server addresses for this server (comma-separated list, e.g. 8.8.8.8,8.8.4.4)", "DNS server addresses")
+$InterfaceAlias = [Microsoft.VisualBasic.Interaction]::InputBox("Enter the Interface Alias for this server (e.g. Ethernet)", "Interface Alias")
+
+# Convert comma-separated list to array
+$dnsServers = $dnsServers -split ","
+
+try {
+    # Set hostname
+    Write-Host "Setting hostname to $hostname ..."
+    Rename-Computer -NewName $hostname -ErrorAction Stop
+    Start-Sleep -Seconds 5
+
+    # Set local IP address
+    Write-Host "Setting local IP address to $localIP ..."
+    try {
+        New-NetIPAddress -InterfaceAlias $InterfaceAlias -IPAddress $localIP -PrefixLength 24 -DefaultGateway $defaultGateway -ErrorAction Stop
+        Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\" -Name "DisabledComponents" -Value "0xffffffff" -Type DWORD
+
+    }
+    catch {
+        if ($_.Exception.Message -like "*not on the same network*") {
+            $newGateway = [Microsoft.VisualBasic.Interaction]::InputBox("The default gateway IP address entered is incorrect or incompatible with the local IP address and prefix length entered. Please enter the correct default gateway IP address.", "Default gateway IP address")
+            New-NetIPAddress -InterfaceAlias $InterfaceAlias -IPAddress $localIP -PrefixLength 24 -DefaultGateway $newGateway -ErrorAction Stop
+        }
+        else {
+            throw $_.Exception
+        }
+    }
+    Set-DnsClientServerAddress -InterfaceAlias $InterfaceAlias -ServerAddresses $dnsServers -ErrorAction Stop
+    Start-Sleep -Seconds 5
+
+    # Set time zone
+    Write-Host "Setting time zone ..."
+    Set-TimeZone -Id "Central European Standard Time" -ErrorAction Stop
+    Start-Sleep -Seconds 1
+
+    # Enable remote desktop access
+    Write-Host "Enabling remote desktop access ..."
+    Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0 -ErrorAction Stop
+    Start-Sleep -Seconds 1
+
+
+    # Disable IE Enhanced Security Setting for Administrators
+    Write-Host "Disabling IE Enhanced Security Setting for Administrators..."
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -ErrorAction Stop
+    Start-Sleep -Seconds 1
+
+
+    # Restart computer
+    Write-Host "Restarting computer ..."
+    Restart-Computer -Force -ErrorAction Stop
+}
+catch {
+    Write-Error "Error occurred during step: $($_.InvocationInfo.ScriptLineNumber). `nError message: $($_.Exception.Message)"
+    Pause
+    exit 1
+}
