e58fb837055dfbd7044ae8e5f5084b2dfcd100e6
LagaeJens <jens.lagae@student.howest.be>
2023-03-25 17:47:07 +0100

    testing

--- Week_2/Script1.ps1
+++ Week_2/Script1.ps1
@@ -1,31 +1,64 @@
 Add-Type -AssemblyName Microsoft.VisualBasic
 
-# Domain name
-$domainName = [Microsoft.VisualBasic.Interaction]::InputBox("Enter the domain name (e.g. intranet.mycompany.be)", "Domain name")
+# promote to domain controller
+$domainname = [Microsoft.VisualBasic.Interaction]::InputBox("Enter the domainname for this server (e.g. intranet.com )")
+Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
+Install-ADDSForest -DomainName $domainname -DomainNetBIOSName AD -InstallDNS
 
-# Make netbios name from domain name take middle part
-$domainNetbiosName = $domainName.Split(".")[1]
-# Make sure the netbios name is not longer than 15 characters
-if ($domainNetbiosName.Length -gt 15) {
-    $domainNetbiosName = $domainNetbiosName.Substring(0, 15)
+# Set ip address and DNS server
+$IPAddress = [Microsoft.VisualBasic.Interaction]::InputBox("Domain controller IP address (e.g. 192.168.1.x)")
+New-NetIPAddress –IPAddress $IPAddress -DefaultGateway 192.168.1.1 -PrefixLength 24 -InterfaceIndex (Get-NetAdapter).InterfaceIndex
+Set-DNSClientServerAddress –InterfaceIndex (Get-NetAdapter).InterfaceIndex –ServerAddresses $IPAddress
+
+
+#variables
+$dcIPAddress = [Microsoft.VisualBasic.Interaction]::InputBox("DNS server IP address nr 1 (e.g. 192.168.1.x)")
+$dc2IPAddress = [Microsoft.VisualBasic.Interaction]::InputBox("DNS server IP address nr 2 (e.g. 192.168.1.x)")
+$preferredDNSServer = $dcIPAddress
+$alternateDNSServer = $dc2IPAddress
+
+# Check and set local DNS servers
+$nic = Get-NetAdapter | Where-Object { $_.Status -eq "Up" -and $_.Name -eq "Ethernet0" } | Select-Object -First 1
+$dnsServers = Get-DnsClientServerAddress -InterfaceIndex $nic.ifIndex | Select-Object -ExpandProperty ServerAddresses
+if ($dnsServers[0] -ne $preferredDNSServer -or $dnsServers[1] -ne $alternateDNSServer) {
+    $dnsServers = @($preferredDNSServer, $alternateDNSServer)
+    Set-DnsClientServerAddress -InterfaceIndex $nic.ifIndex -ServerAddresses $dnsServers
+    Write-Host "Local DNS servers updated."
 }
-# Set the domain netbios name to uppercase
-$domainNetbiosName = $domainNetbiosName.ToUpper()
-
-# Promote the first server to the first DC for the new forest/domain
-Install-ADDSForest -DomainName $domainName -DomainNetbiosName $domainNetbiosName -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText "P@ssw0rd" -Force)
-
-# Check if the necessary role(s) is/are installed. If not, install them.
-$roles = "AD-Domain-Services", "DNS"
-foreach ($role in $roles) {
-    if ((Get-WindowsFeature -Name $role).Installed -ne $true) {
-        Install-WindowsFeature -Name $role -IncludeManagementTools
-    }
+else {
+    Write-Host "Local DNS servers already set correctly."
 }
 
-# Prompt the user to reboot the computer to complete the domain controller installation
-if (Read-Host "The server must be rebooted for the changes to take effect. Do you want to reboot now? (Y/N)" -eq "Y") {
-    Restart-Computer -Force
+Add-DnsServerPrimaryZone -NetworkId "192.168.1.0/24" -ReplicationScope Domain
+Add-DnsServerResourceRecordPtr -Name "10" -ZoneName "1.168.192.in-addr.arpa" -AllowUpdateAny -TimeToLive 01:00:00 -AgeRecord -PtrDomainName "DC1.ad.$domainname"
+
+
+# Variables
+$dhcpScope = "192.168.1.0"
+$dhcpRangeStart = "192.168.1.15"
+$dhcpRangeEnd = "192.168.1.252"
+$dhcpSubnetMask = "255.255.255.0"
+$dhcpRouter = "192.168.1.0"
+$dhcpDNSServers = "192.168.1.2", "192.168.1.3"
+
+# Check if DHCP server role is installed and install if necessary
+if ((Get-WindowsFeature -Name DHCP).Installed -ne "True") {
+    Install-WindowsFeature -Name DHCP
+    Write-Host "DHCP server role installed."
+}
+else {
+    Write-Host "DHCP server role already installed."
 }
 
+# Configure DHCP server and scope
+Add-DhcpServerv4Scope -Name "Main Scope" -StartRange $dhcpRangeStart -EndRange $dhcpRangeEnd -SubnetMask $dhcpSubnetMask -ScopeId $dhcpScope -State Active
+Set-DhcpServerv4OptionValue -OptionId 3 -Value $dhcpRouter -ScopeId $dhcpScope
+Set-DhcpServerv4OptionValue -OptionId 6 -Value $dhcpDNSServers -ScopeId $dhcpScope
+Set-DhcpServerv4OptionValue -OptionId 15 -Value $domainName
 
+# Authorize DHCP server and remove warning in Server Manager
+Add-DhcpServerInDC -DnsName $env:COMPUTERNAME -IPAddress $dcIPAddress
+Set-DhcpServerDnsCredential -Credential (Get-Credential)
+Set-DhcpServerMode -DhcpServerMode "Both"
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\ServerManager\Roles" -Name "PendingXmlIdentifier" -Force
+Write-Host "DHCP server authorized and configured."
