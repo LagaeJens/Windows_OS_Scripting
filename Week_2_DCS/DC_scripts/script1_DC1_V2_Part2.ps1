9483623a883c40ee695a37e54e10727ef241a504
LagaeJens <jens.lagae@student.howest.be>
2023-04-25 14:10:07 +0200

    fix

--- Week_2_DCS/DC_scripts/script1_DC1_V2_Part2.ps1
+++ Week_2_DCS/DC_scripts/script1_DC1_V2_Part2.ps1
@@ -1,40 +1,4 @@
 #
-# "Promote" a standalone server to a Domain Controller (=DC)
-#
-
-$UserDNSDomain = "intranet.mijnschool.be"
-$UserDomain = "mijnschool"
-
-#
-# Install Active Directory Domain Services - ADDS
-#
-
-$WindowsFeature = "AD-Domain-Services"
-if (Get-WindowsFeature $WindowsFeature -ComputerName $env:COMPUTERNAME | Where-Object { $_.installed -eq $false }) {
-    Write-Output "Installing $WindowsFeature ..."
-    Install-WindowsFeature $WindowsFeature -ComputerName $env:COMPUTERNAME -IncludeManagementTools
-}
-else {
-    Write-Output "$WindowsFeature already installed ..."
-}
-
-#
-# Create a new forest and the first DC
-#
-
-Install-ADDSForest `
-    -CreateDnsDelegation:$false `
-    -DatabasePath "C:\windows\NTDS" `
-    -DomainName $UserDNSDomain `
-    -DomainNetbiosName $UserDomain `
-    -InstallDns:$true `
-    -LogPath "C:\windows\NTDS" `
-    -NoRebootOnCompletion:$false `
-    -SysvolPath "C:\windows\SYSVOL" `
-    -Force:$true
-
-
-#
 # Correcting the DNS servers on the active ethernet (802.3) network adapter
 #
 
---
+++ Week_2_DCS/DC_scripts/script2_DC2_V2_Part1.ps1
@@ -0,0 +1,37 @@
+#
+# Promoting DC2 to become an additional DC in the existing Windows domain
+#
+$ComputerName = $env:COMPUTERNAME
+$Credential = $env:USERNAME
+$domainCredential = "$env:USERDOMAIN\$Credential"
+$secondDC = "win00-DC2"
+$UserDNSDomain = $env:USERDNSDOMAIN.tolower()
+
+$remoteSession = New-PSSession -ComputerName $secondDC -Credential $domainCredential
+
+Invoke-Command -Session $remoteSession -Scriptblock {
+
+    #
+    # Installing AD Domain Services - ADDS
+    #
+
+    $WindowsFeature = "AD-Domain-Services"
+    if (Get-WindowsFeature $WindowsFeature -ComputerName $ComputerName | Where-Object { $_.installed -eq $false }) {
+        Write-Output "Installing $WindowsFeature ..."
+        Install-WindowsFeature $WindowsFeature -ComputerName $ComputerName -IncludeManagementTools
+    }
+    else {
+        Write-Output "$WindowsFeature already installed ..."
+    }
+
+    #
+    # Create Domain Controller
+    #
+    Install-ADDSDomainController `
+        -DomainName $args[0] `
+        -InstallDns:$true `
+        -Credential (Get-Credential $args[1]) `
+        -Force:$true
+
+} -ArgumentList $UserDNSDomain, $domainCredential
+
