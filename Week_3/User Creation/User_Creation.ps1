41ee90163350914d0cac7c80dbbe1b0ed6d77f1f
LagaeJens <jens.lagae@student.howest.be>
2023-04-28 12:28:13 +0200

    added creation fixes

---
+++ Week_3/User Creation/User_Creation.ps1
@@ -0,0 +1,54 @@
+#
+# Creating users in AD
+#
+# *** Make sure the root folder for the user home folders already exists and is already shared
+#
+
+$userNames = Import-Csv ".\UserAccounts.csv" -Delimiter ";"
+
+$UPNSuffix = 'mijnschool.be'
+
+$homeServer = "win00-ms"
+$homeShare = "Homedirs"
+
+Foreach ($User in $userNames) { 
+    $userName = $User.Name
+    $samAccountName = $User.SamAccountName
+    $userPrincipalName = $User.Name + "@" + $UPNSuffix
+    $displayName = $User.DisplayName
+    $givenName = $User.GivenName
+    $surName = $User.SurName
+    $homeDrive = $User.HomeDrive
+    $homeDirectory = "\\" + $homeServer + "\" + $homeShare + "\" + $User.Name
+    $objectPath = $User.Path
+
+    $accountPassword = ConvertTo-SecureString $User.AccountPassword -AsPlainText -force
+
+    try {
+        Get-ADUser -identity $samAccountName | Out-Null
+        Write-Output "$Name already exists in $Path!"
+    }
+    catch {
+        Write-Output "Making $User.Name in $Path ..." 
+
+        New-ADUser -Name $userName -SamAccountName $samAccountName -UserPrincipalName $userPrincipalName -DisplayName $displayName -GivenName $givenName -Surname $surName -HomeDrive $homeDrive -HomeDirectory $homeDirectory -Path $objectPath -AccountPassword $accountPassword -Enabled:$true
+	    
+        New-Item -Path $homeDirectory -type directory -Force
+    
+        $acl = Get-Acl $homeDirectory
+
+        # Enable inheritance and copy permissions
+        $acl.SetAccessRuleProtection($False, $True)
+        # Setting Modify for the User account
+        $Identity = $userPrincipalName
+        $Permission = "Modify"
+        $Inheritance = "ContainerInherit, ObjectInherit"
+        $Propagation = "None"
+        $AccessControlType = "Allow"
+        $rule = New-Object System.Security.AccessControl.FileSystemAccessRule
+	        ($Identity, $Permission, $Inheritance, $Propagation, $AccessControlType)
+        $acl.AddAccessRule($rule)
+ 
+        Set-Acl $HomeDirectory $acl
+    }
+}
