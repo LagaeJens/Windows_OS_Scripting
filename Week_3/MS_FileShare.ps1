05a46b446a2e81375e73459e3a3168cb3d433950
LagaeJens <jens.lagae@student.howest.be>
2023-04-28 10:11:25 +0200

    ms

---
+++ Week_3/MS_FileShare.ps1
@@ -0,0 +1,108 @@
+#
+# Making the root share that will contain the user home folders
+#
+
+$fileServer = "win15-ms"
+
+$systemShare = "C$"
+$driveLetter = $systemShare.replace("$", ":")
+$rootShare = "Homedirs"
+$localPath = $driveLetter + "\" + $rootShare
+$UNCPath = "\\" + $fileServer + "\" + $systemShare + "\" + $rootShare
+
+if (Get-Item -Path $UNCPath -ErrorAction SilentlyContinue) {
+    Write-Output “$UNCPath already exists ..."   
+}
+else {
+    Write-Output "Creating $UNCPath ..." 
+    New-Item -Path $UNCPath -type directory -Force | Out-Null
+}
+
+if (Get-SmbShare -CimSession $fileServer -Name $rootShare -ErrorAction SilentlyContinue) {
+    Write-Output “$localPath already shared ..."
+}
+else {
+    Write-Output "Sharing $localPath on $fileServer as $rootShare ..." 
+    New-SmbShare -CimSession $fileServer -Name $rootShare -Path $localPath -FullAccess Everyone | Out-Null
+
+    $acl = Get-Acl $UNCPath
+
+    # Disable inheritance and remove all permissions
+    $acl.SetAccessRuleProtection($True, $False)
+
+    # Setting Full Control for Administrators
+    $Identity = "Administrators"
+    $Permission = "FullControl"
+    $Inheritance = "ContainerInherit, ObjectInherit"
+    $Propagation = "None"
+    $AccessControlType = "Allow"
+    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule
+		($Identity, $Permission, $Inheritance, $Propagation, $AccessControlType)
+    $acl.AddAccessRule($rule)
+
+    # Setting Read & Execute for Authenticated Users on This Folder only
+    $Identity = "Authenticated Users"
+    $Permission = "ReadAndExecute"
+    $Inheritance = "None"
+    $Propagation = "NoPropagateInherit"
+    $AccessControlType = "Allow"
+    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule
+		($Identity, $Permission, $Inheritance, $Propagation, $AccessControlType)
+    $acl.AddAccessRule($rule)
+
+    Set-Acl $UNCPath $acl
+}
+#
+# Making the root share for storing the roaming user profiles
+#
+
+$fileServer = "win15-ms"
+
+$systemShare = "C$"
+$driveLetter = $systemShare.replace("$", ":")
+$rootShare = "Profiles$"
+$localPath = $driveLetter + "\" + $rootShare
+$UNCPath = "\\" + $fileServer + "\" + $systemShare + "\" + $rootShare
+
+if (Get-Item -Path $UNCPath -ErrorAction SilentlyContinue) {
+    Write-Output "$UNCPath already exists ..."   
+}
+else {
+    Write-Output "Creating $UNCPath ..." 
+    New-Item -Path $UNCPath -type directory -Force | Out-Null
+}
+
+if (Get-SmbShare -CimSession $fileServer -Name $rootShare -ErrorAction SilentlyContinue) {
+    Write-Output "$localPath already shared on $fileServer ..."
+}
+else {
+    Write-Output "Sharing $localPath on $fileServer as $rootShare ..." 
+    New-SmbShare -CimSession $fileServer -Name $rootShare -Path $localPath -FullAccess Everyone | Out-Null
+
+    $acl = Get-Acl $UNCPath
+
+    # Disable inheritance and remove all permissions
+    $acl.SetAccessRuleProtection($True, $False)
+
+    # Setting Full Control for Administrators
+    $Identity = "Administrators"
+    $Permission = "FullControl"
+    $Inheritance = "ContainerInherit, ObjectInherit"
+    $Propagation = "None"
+    $AccessControlType = "Allow"
+    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule
+    ($Identity, $Permission, $Inheritance, $Propagation, $AccessControlType)
+    $acl.AddAccessRule($rule)
+
+    # Setting Modify for Authenticated Users
+    $Identity = "Authenticated Users"
+    $Permission = "Modify"
+    $Inheritance = "ContainerInherit, ObjectInherit"
+    $Propagation = "None"
+    $AccessControlType = "Allow"
+    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule
+	    ($Identity, $Permission, $Inheritance, $Propagation, $AccessControlType)
+    $acl.AddAccessRule($rule)
+
+    Set-Acl $UNCPath $acl
+}
